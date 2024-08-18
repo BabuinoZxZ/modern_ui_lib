b2870729e12129d4f6b9dda74c0f5aef6bc8daa7
BabuinoZxZ <119818119+BabuinoZxZ@users.noreply.github.com>
2024-08-17 21:34:31 -0300

    Delete modern_ui_lib directory

--- modern_ui_lib/modern_ui_lib/widgets.py
+++
@@ -1,7 +0,0 @@
-# modern_ui_lib/widgets.py
-from .base import BaseWidget
-
-class ModernButton(BaseWidget):
-    def __init__(self, master=None, text='', **kwargs):
-        super().__init__(master, **kwargs)
-        self.config(text=text)
